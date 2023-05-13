d5cd166f7e0ca004356cb0ef7d10a505ac84a275
gaganKUMMUR <62560026+gaganKUMMUR@users.noreply.github.com>
2023-05-13 08:12:27 +0530

    Update chart.py

--- chart.py
+++ chart.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-from scipy.interpolate import interp1d
 
 st.title("Chemical reaction Engineering")
 st.header("Method to find the order of Reaction of the given concentration by Linear Fitting")
