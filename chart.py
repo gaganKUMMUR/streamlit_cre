90f38d58c3c72cff2c14c0ee63ff4a6f564bf729
gaganKUMMUR <62560026+gaganKUMMUR@users.noreply.github.com>
2023-05-13 08:11:59 +0530

    Update chart.py

--- chart.py
+++ chart.py
@@ -3,9 +3,11 @@ import pandas as pd
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+
 st.title("Chemical reaction Engineering")
 st.header("Method to find the order of Reaction of the given concentration by Linear Fitting")
-
+st.write("This website is dedicated to the collection and analysis of data. Our primary focus lies in employing numerical methods to calculate the rate of reactions, enabling us to estimate both the rate constant and the order of reaction.")
 t = [0,1,2,3,4]
 n = st.number_input('Insert number of reading',5,10)
 for i in range(5,int((n+1))):
@@ -19,14 +21,13 @@ ipt=1
 def rate_calculator(n,b,a):
   c=(-3*f[0]+4*f[1]-f[2])/2
   b.append(c)
-  for i in range (n-1):
+  for i in range((n-1)):
     c=(f[i+2]-f[i])/2
     b.append(c)
   b.append((f[n-2]-4*f[n-1]+3*f[n])/2)
   print (b)
   for i in range (n+1):
       b[i]=math.log(-b[i])
-  plot_data(t,b, "rate of reaction vs time", 'rate of reaction in mol/(m^3*minute)')
   plot_data(t,f, "concetration vs time", 'concetration in mol/m^3')
   return
 
@@ -42,16 +43,20 @@ def fit_line_equation(x, y):
     print (equation)
     return coefficients
 
-def plot_data(x, y,name,yaxis, xaxis='time in minute'):
+def plot_data(x, y,name,yaxis, xaxis='time in sec'):
     # Plot the data points
+    
     fig, ax = plt.subplots() 
     ax.scatter(x, y, color='blue')
+
     ax.set_xlabel(xaxis)
     # ax.set_xlim(xmin=0)
     ax.set_ylabel(yaxis)
     # ax.set_ylim(ymin=0)
     ax.set_title(name)
     ax.grid(True)
+    for i, txt in enumerate(y):
+        ax.annotate("%0.2f" %(txt), (x[i], y[i]))
     st.pyplot(fig)
     
 def plot_line(slope, intercept):
@@ -80,25 +85,25 @@ def plot_line(slope, intercept):
 ca0 = st.number_input('initial concentration',15.00)
 a.append(ca0)
 d = [st.number_input(f'enter concentration after {str(i+1)} miniutes', value=ca0-i-2, max_value=ca0,key=f"text_input_{i}") for i in range(n)]
-# c = [ st.number_input(f'enter concentration after {str(i+1)} miniutes') i for i in range(n)] 
-# st.number_input("enter concentration after "+ str(1) + "miniutes")
-# a.append(c)
-# for i in range (int(n-1)):
-#     # print ("enter concentration after "+ str(i+1) + "miniutes")
-#     # c=float(input())
-#     c = st.number_input("enter concentration after "+ str(i+2) + "miniutes")
-#     a.append(c)
+g=[0,0,0,0,0,0]
+m= []
 if len(d) !=0:
    f = a+d
 
+for i in range(n+1):
+   m.append(math.log(f[i]))
+
+if len(m) ==0:
+   m = g
 
 print ("a", f)
-
+print("g",m)
 rate_calculator(n,b,a)
+plot_data(m,b, "-ln(d(ca)/dt) vs ln(ca)", xaxis='ln(ca)',yaxis= '-ln(d(ca)/dt)')
 
 
 
-coefficients = fit_line_equation(t,b)
+coefficients = fit_line_equation(m,b)
 plot_line(coefficients[0],coefficients[1])
 st.write("Order of the reaction is : %f" %(coefficients[0]))
 print ("order of the reaction is:")
